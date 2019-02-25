6e0d26fdfbeb766687ce7b08f38ca279790aba4e
ybaravy <ybaravy@gmail.com>
2019-02-25 23:52:50 +0300

    Add clients logos with initial visualisation

--- src/components/ClientsSection/index.js
+++ src/components/ClientsSection/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import akd from '../../static/akd-small.jpg';
+import be from '../../static/be-small.jpg';
+import bsb from '../../static/bsb-small.jpg';
+import gsb from '../../static/gsb-small.jpg';
+import sek from '../../static/sek-small.png';
 
-let Clients = (props) => {
-	return (
-		<section id="clients">
+export default class Clients extends Component {
+	componentDidMount() {
+		let slides = document.querySelectorAll('#slides .slide');
+		let currentSlide = 0;
+		let slideInterval = setInterval(nextSlide,2000);
 		
-		</section>
-	)
+		console.log(slides);
+		
+		function nextSlide() {
+			slides[currentSlide].className = 'slide';
+			currentSlide = (currentSlide+1)%slides.length;
+			slides[currentSlide].className = 'slide showing';
+		}
+	}
+	
+	render() {
+		return (
+			<section id="clients">
+				<ul id="slides">
+					<li className="slide showing">
+						<img src={akd} />
+					</li>
+					<li className="slide">
+						<img src={be} />
+					</li>
+					<li className="slide">
+						<img src={bsb} />
+					</li>
+					<li className="slide">
+						<img src={gsb} />
+					</li>
+					<li className="slide">
+						<img src={sek} />
+					</li>
+				</ul>
+			</section>
+		)
+	}
 };
-
-export default Clients;
