02618bbc1c33889346d7f9b6bdf984bd08f5abd9
ybaravy <ybaravy@gmail.com>
2019-03-04 00:22:40 +0300

    Separate mobile menu to it's own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import ServicesSection from '../ServicesSection/index';
 import ContactUsSection from "../ContactUsSection";
 import ClientsSection from "../ClientsSection";
 import BannerSection from "../BannerSection";
+import MobileMenu from "../MobileMenu";
 
 class App extends Component {
   constructor(props) {
@@ -44,16 +45,7 @@ class App extends Component {
       <div className={"app " + (activeMobileMenu ? "opened" : "")}>
 				{
 					activeMobileMenu ?
-						<section
-							className="side-menu"
-						>
-              <p>ИП Такушевич</p>
-              <a href="/">Главная</a>
-              <a href="#about">О Нас</a>
-              <a href="#services">Услуги</a>
-              <a href="#clients">Наши Заказчики</a>
-              <a href="#contact-us">Контакты</a>
-            </section> :
+						<MobileMenu /> :
 						null
 				}
 				<div className={activeMobileMenu ? "menu-show" : ""}>
---
+++ src/components/MobileMenu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+let MobileMenu = (props) => {
+	return (
+		<section
+			className="side-menu"
+		>
+			<p>ИП Такушевич</p>
+			<a href="/">Главная</a>
+			<a href="#about">О Нас</a>
+			<a href="#services">Услуги</a>
+			<a href="#clients">Наши Заказчики</a>
+			<a href="#contact-us">Контакты</a>
+		</section>
+	)
+};
+
+export default MobileMenu;
