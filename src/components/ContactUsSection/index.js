53d28316ac12bdd4eed6de9e78c912678d9532ed
Yury Baravy <ybaravy@gmail.com>
2019-08-17 23:12:56 +0300

    Implement functionality to send form data using SNS

--- src/components/ContactUsSection/index.js
+++ src/components/ContactUsSection/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-const ContactUsSection = (props) => (
-  <section id="contact-us">
-    <header>Не можете ввести объект в эксплуатацию?</header>
-    <p>Закажите проект прямо сейчас и получите консультацию от наших специалистов!</p>
-    <form>
-      <input type="text" placeholder="Ваш email"/>
-      <button type="submit">Заказать проект</button>
-    </form>
-  </section>
-);
+export default class ContactUsSection extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTypeReciever = this.handleTypeReciever.bind(this);
+    this.state = {
+      reciever: ''
+    }
+  }
 
-export default ContactUsSection;
+  handleTypeReciever(event) {
+    let reciever = event.target.value;
+    this.setState({ reciever })
+  }
+
+  handleSubmit(event) {
+    const payload = {
+      email: this.state.reciever,
+    };
+    console.dir(payload);
+    const API_URL = "https://lk480bz0ef.execute-api.eu-west-1.amazonaws.com/contuct-us";
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'text/plain'
+      }
+    })
+      .then(res => console.log(res))
+      .catch(e => console.error(e.message));
+    event.preventDefault();
+  }
+
+  render() {
+    const recieverEmail = this.state.reciever;
+    return (
+      <section id="contact-us">
+        <header>Не можете ввести объект в эксплуатацию?</header>
+        <p>Закажите проект прямо сейчас и получите консультацию от наших специалистов!</p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Ваш email" value={recieverEmail} onChange={this.handleTypeReciever}/>
+          <button type="submit">Заказать проект</button>
+        </form>
+      </section>
+    )
+  }
+};
