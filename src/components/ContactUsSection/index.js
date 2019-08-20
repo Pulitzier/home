ebaf451723133097effb68712f98992d8825c1d7
Yury Baravy <ybaravy@gmail.com>
2019-08-20 23:57:47 +0300

    Update Contuct Us component with notifications

--- src/components/ContactUsSection/index.js
+++ src/components/ContactUsSection/index.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import './style.css';
+import add from '../../static/media/add.svg';
+
+const INITIAL_STATE = {
+  reciever: '',
+  error: false,
+  success: false,
+  errorObj: {}
+};
 
 export default class ContactUsSection extends Component {
   constructor() {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleTypeReciever = this.handleTypeReciever.bind(this);
-    this.state = {
-      reciever: ''
-    }
+    this.state = INITIAL_STATE;
   }
 
-  handleTypeReciever(event) {
+  handleTypeReciever = (event) => {
     let reciever = event.target.value;
     this.setState({ reciever })
-  }
+  };
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     const payload = {
       "email": this.state.reciever,
     };
-    // console.dir(payload);
     const API_URL = "https://lk480bz0ef.execute-api.eu-west-1.amazonaws.com/contuct-us/handleSendNotification";
+
+    document.addEventListener('click', this.clearTimeoutAndSetState);
+
     fetch(API_URL, {
       method: 'POST',
       headers: {
@@ -30,15 +36,53 @@ export default class ContactUsSection extends Component {
       },
       body: JSON.stringify(payload)
     })
-      .then(res => console.log(res))
-      .catch(e => console.error(e.message));
+      .then(res => {
+        if(res.status == 200) {
+          this.setStateAndTimeout({ reciever: '', success: true })
+        } else {
+          this.setStateAndTimeout({ reciever: '', error: true, errorObj: res })
+        }
+      })
+      .catch(e => this.setStateAndTimeout({ error: true, errorObj: e }));
     event.preventDefault();
-  }
+  };
+
+  setStateAndTimeout = (opts) => {
+    this.setState(opts);
+    this.notificationTimeout = setTimeout(() => this.setState(INITIAL_STATE), 3000);
+  };
+
+  clearTimeoutAndSetState = () => {
+    clearTimeout(this.notificationTimeout);
+    this.setState(INITIAL_STATE);
+  };
+
+  renderNotificationMessage = () => {
+    const { success: successMessage, error: errorMessage } = this.state;
+    return (
+      <div className={`notification ${ errorMessage ? 'failure' : successMessage ? 'success' : ''}`}>
+        <div className="notification-card">
+          <button className="close-notification" onClick={this.clearTimeoutAndSetState}>
+            <img src={add} alt="Close notification sign"/>
+          </button>
+          {
+            errorMessage ?
+              <p>Что-то пошло не так! Приносим свои извинения.</p> :
+              successMessage ?
+                <p>Ваш email зарегистрирован успешно!</p> :
+                null
+          }
+        </div>
+      </div>
+  )};
 
   render() {
-    const recieverEmail = this.state.reciever;
+    const { reciever: recieverEmail, success, error } = this.state;
     return (
       <section id="contact-us">
+        {
+          (error || success) ? this.renderNotificationMessage() : null
+        }
         <header>Не можете ввести объект в эксплуатацию?</header>
         <p>Закажите проект прямо сейчас и получите консультацию от наших специалистов!</p>
         <form onSubmit={this.handleSubmit}>
