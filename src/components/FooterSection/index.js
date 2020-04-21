744aaf3cde6e93a3d886abf784037efff6a0df3d
Yury Baravy <ybaravy@gmail.com>
2020-04-21 11:41:30 +0300

    Update mobile styles

--- src/components/FooterSection/index.js
+++ src/components/FooterSection/index.js
@@ -8,8 +8,8 @@ const FooterSection = () => (
     <p>Республика Беларусь, г. Минск</p>
     <p><b>Телефон:</b> +375293640264</p>
     <p><b>Email:</b> <a href="mailto:tak.ef@mail.ru">tak.ef@mail.ru</a></p>
-    <p>Режим работы: пн-пт 08.00 - 19.00</p>
-    <header>Мы в социальных сетях</header>
+    <p>Режим работы: пн-вс 08.00 - 19.00</p>
+    <p className="header">Мы в социальных сетях</p>
     <SocialIcons dimensions={{ width: 40, height: 40 }} />
   </section>
 );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SocialIcons from '../SocialIconsList';
 import './style.css';
 
-const Header = ({ toggleMenu, activeMobileMenu }) => {
- return (
-  <header className={"brand-header-wrapper " + (activeMobileMenu ? "menu-activated" : '')}>
-    <div className="brand-header">
-        <p className="brand-headline">ИП Такушевич</p>
-        <section className="side-menu" >
-            <a href="#about">О нас</a>
-            <a href="#what-we-do">Чем мы занимаемся?</a>
-            <a href="#our-benefits">Наши преимущества</a>
-            <a href="#project-structure">Проект</a>
-            <a href="#partners">Партнеры</a>
-            <a href="#contact-us">Контакты</a>
-            <SocialIcons dimensions={{ width: 24, height: 24 }} />
-       </section>
-        <button
-            className={"btn-toggle-control " + (activeMobileMenu ? "toggled-menu" : '')}
-            onClick={toggleMenu}
-        >
-        <div className="hamb-button">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </button>
-    </div>
-  </header>
-)};
+export default class Header extends Component {
+  bindEventsListener() {
+    let brandHeaderLinks = document.getElementsByClassName("brand-header-link");
+    Array.from(brandHeaderLinks).forEach(
+      item => item.addEventListener('click', () => this.props.toggleMenu())
+    );
+  };
 
-export default Header;
+  componentDidMount() {
+    this.bindEventsListener();
+  }
+
+  render() {
+    const { toggleMenu, activeMobileMenu } = this.props;
+    return (
+      <header className={"brand-header-wrapper " + (activeMobileMenu ? "menu-activated" : '')}>
+        <div className="brand-header">
+            <p className="brand-headline">ИП Такушевич</p>
+            <section className="side-menu" >
+              <a href="#about" className="brand-header-link">О нас</a>
+              <a href="#what-we-do" className="brand-header-link">Чем мы занимаемся?</a>
+              <a href="#our-benefits" className="brand-header-link">Наши преимущества</a>
+              <a href="#project-structure" className="brand-header-link">Проект</a>
+              <a href="#partners" className="brand-header-link">Партнеры</a>
+              <a href="#contact-us" className="brand-header-link">Контакты</a>
+              <SocialIcons dimensions={{ width: 24, height: 24 }} />
+           </section>
+            <button
+              className={"btn-toggle-control " + (activeMobileMenu ? "toggled-menu" : '')}
+              onClick={toggleMenu}
+            >
+            <div className="hamb-button">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </button>
+        </div>
+      </header>
+    )
+  }
+};
